e7642576747636e1d8964d15186f88b35c2cad84
mrkaye97 <mrkaye97@gmail.com>
2021-01-01 22:11:51 -0500

    cleanup

--- R/helpers.R
+++ R/helpers.R
@@ -8,3 +8,24 @@ ut <- function(pack) {
   use_package(pack, min_version = packageVersion(pack))
   return(invisible(NULL))
 }
+
+#' multiples_sum
+#' get the sum of the multiples of the numbers specified less than the max value specified
+#' @param numbers the numbers whose multiples to include in the sum
+#' @param max_value the largest number to consider
+#' @return the sum
+#' @importFrom tibble tibble
+#' @importFrom magrittr %>%
+#' @importFrom purrr reduce map
+#' @import dplyr
+multiples_sum <- function(numbers = c(3,5), max_value = 1000) {
+  numbers %>%
+    map(~ tibble(number = 1:(max_value-1)) %>%
+          mutate(s = ifelse(.data$number %% .x == 0, .data$number, 0))) %>%
+    reduce(left_join, by = 'number') %>%
+    rowwise() %>%
+    filter(any(c_across(-.data$number) == .data$number)) %>%
+    ungroup() %>%
+    summarize(x = sum(.data$number)) %>%
+    pull(.data$x)
+}
--- R/solutions.R
+++ R/solutions.R
@@ -1,21 +1,8 @@
 #' Problem 1
 #' If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #' Find the sum of all the multiples of 3 or 5 below 1000.
-#' @param numbers the numbers whose multiples to include in the sum
-#' @param max_value the largest number to consider
 #' @return the sum
-#' @importFrom tibble tibble
-#' @importFrom magrittr %>%
-#' @importFrom purrr reduce map
-#' @import dplyr
-multiples_sum <- function(numbers = c(3,5), max_value = 1000) {
-  numbers %>%
-    map(~ tibble(number = 1:(max_value-1)) %>%
-          mutate(s = ifelse(.data$number %% .x == 0, .data$number, 0))) %>%
-    reduce(left_join, by = 'number') %>%
-    rowwise() %>%
-    filter(any(c_across(-.data$number) == .data$number)) %>%
-    ungroup() %>%
-    summarize(x = sum(.data$number)) %>%
-    pull(.data$x)
+problem_1 <- function() {
+  multiples_sum(numbers = c(3,5), max_value = 1000)
 }
+
