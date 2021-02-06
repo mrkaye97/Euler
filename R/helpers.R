046a0d9703bd0365d3db62f82d92587d535bb9c4
mrkaye97 <mrkaye97@gmail.com>
2021-02-05 19:24:35 -0700

    problem 22

--- R/helpers.R
+++ R/helpers.R
@@ -106,3 +106,55 @@ get_squares <- function(n_max = 10, type = 'all') {
     map_dbl(~ .x ** 2)
 }
 
+#' letter_to_number
+#'
+#' convert a letter to a number
+#' @param letter the letter to convert
+#' @return the number
+letter_to_number <- function(letter) {
+  switch(
+    letter,
+    "A" = 1,
+    "B" = 2,
+    "C" = 3,
+    "D" = 4,
+    "E" = 5,
+    "F" = 6,
+    "G" = 7,
+    "H" = 8,
+    "I" = 9,
+    "J" = 10,
+    "K" = 11,
+    "L" = 12,
+    "M" = 13,
+    "N" = 14,
+    "O" = 15,
+    "P" = 16,
+    "Q" = 17,
+    "R" = 18,
+    "S" = 19,
+    "T" = 20,
+    "U" = 21,
+    "V" = 22,
+    "W" = 23,
+    "X" = 24,
+    "Y" = 25,
+    "Z" = 26,
+    "I didn't recognize that letter!"
+  )
+}
+
+#' get_word_values
+#'
+#' convert a word to a number based on the num of its letters
+#' @param word the letter to convert
+#' @importFrom purrr map_dbl
+#' @return the number
+get_word_values <- function(word) {
+  word %>%
+    map_dbl(letter_to_number) %>%
+    sum()
+}
+
+
+
